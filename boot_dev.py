bbf9c99a832e671355ec14ee79714184fdce62d3
bneil006 <brmneil2@gmail.com>
2025-02-11 18:28:56 -0600

    CH3: Abstraction | C2: Deck of Cards

--- boot_dev.py
+++ boot_dev.py
@@ -1,27 +1,58 @@
-class Student:
-    def __init__(self, name):
-        self.name = name
-        self.__courses = {}
+import random
 
-    def calculate_letter_grade(self, score):
-        if score >= 90: return "A"
-        elif score >= 80: return "B"
-        elif score >= 70: return "C"
-        elif score >= 60: return "D"
-        else: return "F"
+class DeckOfCards:
+    SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    RANKS = [
+        "Ace",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "Jack",
+        "Queen",
+        "King",
+    ]
+    def __init__(self):
+        self.__cards = self.create_deck()
 
-    def add_course(self, course_name, score):
-        self.__courses[course_name] = self.calculate_letter_grade(score)
+    def create_deck(self):
+        result = []
+        for suit in DeckOfCards.SUITS:
+            for rank in DeckOfCards.RANKS:
+                card = (rank, suit)
+                result.append(card)
+        return result
 
-    def get_courses(self):
-        return self.__courses
+    def shuffle_deck(self):
+        random.shuffle(self.__cards)
+
+    def deal_card(self):
+        if len(self.__cards) > 0:
+            card = self.__cards[-1]
+            self.__cards.pop()
+            return card
+        else:
+            return None
+
+    # don't touch below this line
+
+    def __str__(self):
+        return f"The deck has {len(self.__cards)} cards"
 
 
 def main():
-    learner = Student("Brandon")
-    learner.add_course("Science", 76)
-    learner.add_course("English", 87)
-    print(learner.get_courses())
+    a = DeckOfCards()
+    a.shuffle_deck()
+
+    for _ in range(0, 53):
+        card = a.deal_card()
+        print(card)
+
 
 
 main()
